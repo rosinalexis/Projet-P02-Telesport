f6e9823ee4ec4f9d826e86ca6e5fe40a9686fee7
rosinalexis <rosinalexis@yahoo.fr>
2024-05-18 20:48:56 +0200

    P02/Page-Details : ajout de la page de detail

---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Graph } from 'src/app/core/models/Graph';
+import { Olympic } from 'src/app/core/models/Olympic';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.scss']
+})
+export class DetailComponent implements OnDestroy, OnInit {
+
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  showLegend = false;
+  showXAxisLabel = true;
+  xAxisLabel = 'Years';
+  showYAxisLabel = true;
+  yAxisLabel = 'Medals';
+
+  olympic!: Olympic;
+  numberOfMedal !: number;
+  numberOfAthlete!: number;
+  olympicFormatData: Graph[] = [];
+
+  subscription!: Subscription;
+
+  constructor(private router: ActivatedRoute, private olympicService: OlympicService) {
+
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    const id: string | null = this.router.snapshot.paramMap.get('id');
+    if (id) {
+      this.subscription = this.olympicService.getOlympicByCountryById(+id).subscribe((res) => {
+        this.olympic = res;
+        this.numberOfMedal = this.calculateTotalOfMedal(this.olympic);
+        this.numberOfAthlete = this.calculateTotalOfAthlete(this.olympic);
+        this.olympicFormatData = this.calculateMedalsCountByYears();
+
+      })
+    }
+  }
+
+  calculateTotalOfMedal(olympic: Olympic): number {
+
+    return olympic.participations.reduce(
+      (total: number, participation: { medalsCount: number; }) => total + participation.medalsCount, 0);
+
+  }
+
+  calculateTotalOfAthlete(olympic: Olympic): number {
+
+    return olympic.participations.reduce(
+      (total: number, participation: { athleteCount: number; }) => total + participation.athleteCount, 0);
+
+  }
+
+  calculateMedalsCountByYears(): Graph[] {
+
+    return this.olympic?.participations.map(participation => ({
+      name: participation.year.toString(),
+      value: participation.medalsCount
+    }));
+
+  }
+
+
+
+}
